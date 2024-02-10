89e63c3c0de2c6b55a3a7814e368461cbffccd23
ivpenny <202010865@btech.ph.education>
2024-02-10 16:54:21 +0800

    UI update

--- resources/views/user/reservations/form.blade.php
+++ resources/views/user/reservations/form.blade.php
@@ -44,7 +44,7 @@
             <tr>
             <td>
               <div class="input-group mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-default">Venue Address</span>
+                <span class="input-group-text" id="inputGroup-sizing-default">Event Address</span>
                 <input type="text" class="form-control"  name="venue_address" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
               </div>
             </td>
